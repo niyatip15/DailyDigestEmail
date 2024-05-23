148231559f82486becea495488d7a215d860a309
niyatip15 <niyupathak1503@gmail.com>
2024-05-23 15:51:42 +0530

    Added Links, Specified Language, Limited news upto 20, Added Subject and controlled the Category in url.

--- main.py
+++ main.py
@@ -2,15 +2,30 @@ import requests
 from send_news_email import send_email
 
 api_key = "34769740308a450ebfa7cddb6aa73b2d"
-url = "https://newsapi.org/v2/everything?q=tesla&"\
-    "from=2024-04-23&sortBy=publishedAt&apiKey=34769740308a450ebfa7cddb6aa73b2d"
 
+topic = "tesla"
+url = f"https://newsapi.org/v2/everything?q={topic}&"\
+    "from=2024-04-23&sortBy=publishedAt&apiKey=34769740308a450ebfa7cddb6aa73b2d&language=en"
+
+# Make a request 
 request = requests.get(url)
+
+# Get dictionary with data 
 content = request.json()
-body = ""
-for article in content["articles"]:
+
+# Initialize the body with the Subject line
+body = "Subject: Today's News\n\n"
+
+# Access the title, article, description, and URL
+for article in content["articles"][:20]:
     title = article["title"]
     description = article["description"] if article["description"] else ""
-    body = body + title + "\n" + description + "\n\n"
-body= body.encode("utf-8")
+    url = article["url"]
+    # Append each article's details to the body
+    body += f"{title}\n{description}\n{url}\n\n"
+
+# Encode the message as UTF-8 before sending
+body = body.encode("utf-8")
+
+# Send the email
 send_email(message=body)
--- send_news_email.py
+++ send_news_email.py
@@ -3,9 +3,9 @@ import smtplib,ssl,os
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
-    username = "abc@gmail.com"
+    username = "niyupathak1503@gmail.com"
     app_password = os.getenv("PASSWORD")
-    receiver = "abc@gmail.com"
+    receiver = "niyupathak1503@gmail.com"
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, app_password)
